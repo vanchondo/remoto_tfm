120b4f5f6c9ea85339611f7e4ae967659c385d9f
Victor Anchondo <eduardoanchondo@gmail.com>
2022-07-24 10:09:04 -0600

    Adding spring profile properties and encrypted mongo credentials

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # remoto_tfm
+
+Run locally:
+gradle bootRun -Pspring.profiles.active=local
+
+Run with dev profile:
+gradle bootRun -Pspring.profiles.active=dev -Pjasypt.encryptor.password=<secretKey>
--- src/main/java/com/vanchondo/tfm/Application.java
+++ src/main/java/com/vanchondo/tfm/Application.java
@@ -1,11 +1,13 @@
 package com.vanchondo.tfm;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEncryptableProperties
 public class Application {
 
     public static void main(String[] args){
